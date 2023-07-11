60309a43eb74c0659102e4b978bfe059d429a565
snowfdb <139226739+snowfdb@users.noreply.github.com>
2023-07-11 16:50:52 +0530

    Update streamlit_app.py
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamli.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍵 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
