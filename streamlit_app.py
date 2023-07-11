45efec6363bfc7dab70e16604932aebf1f290c55
snowfdb <139226739+snowfdb@users.noreply.github.com>
2023-07-11 16:46:15 +0530

    Update streamlit_app.py
    added emoji to the menu list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamli.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍵 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 Avacado Toast')
